322554eb6e533c0ccf32640125b627ab5e7017a7
Saad Hossain <mshsarkar@gmail.com>
2023-02-21 11:02:57 +0600

    Footer updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='bg-gray-100 border border-gray-200 py-2 text-gray-800 font-poppins text-center'>
-            <p>&copy;2023 Allrights Reserved | Developed By <span className='text-actionbtn'><a href='https://saadhossain.dev/'>Saddam Hossain</a></span></p>
+            <p>&copy;2023 FreJun | Developed By <span className='text-actionbtn'><a href='https://saadhossain.dev/'>Saddam Hossain</a></span></p>
         </div>
     );
 };
